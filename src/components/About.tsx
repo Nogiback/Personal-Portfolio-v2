e3e75d7b12ebeface7fd85eeaef3b31a2f74c30b
Peter Do <peter.toan.do@gmail.com>
2024-06-05 20:50:02 -0400

    Starting About section of webpage

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default function About() {
   return (
     <div id='About' className='flex h-screen items-center justify-center'>
-      About
+      <div className='flex flex-col gap-6'>
+        <p>Hello. My name is Peter Do.</p>
+      </div>
     </div>
   );
 }
