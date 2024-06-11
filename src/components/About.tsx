79438b9e9cb5eadd88e2dce0dcb8900c9fad8513
Peter Do <peter.toan.do@gmail.com>
2024-06-11 19:03:37 -0400

    Creating section title component, refactoring sections with new section title component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,5 @@
+import SectionTitle from './ui/SectionTitle';
+
 export default function About() {
   return (
     <div
@@ -5,11 +7,7 @@ export default function About() {
       className='flex h-screen w-full items-center justify-center px-12 sm:px-24'
     >
       <div className='flex w-full flex-col gap-6'>
-        <div className='flex w-full items-center justify-center pb-4 after:relative after:ml-4 after:block after:h-[1px] after:w-full after:bg-accent after:content-[""]'>
-          <h2 className='min-w-36 font-secondary text-lg font-bold sm:min-w-48 sm:text-2xl'>
-            <span className='text-accent'>01.</span> About Me
-          </h2>
-        </div>
+        <SectionTitle number='01.' title='About Me' />
         <p>
           Hello. My name is Peter Do. Former Engineer. Current Web Developer.
         </p>
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,5 @@
+import SectionTitle from './ui/SectionTitle';
+
 export default function Contact() {
   return (
     <div
@@ -5,14 +7,10 @@ export default function Contact() {
       className='relative flex h-screen w-full items-center justify-center bg-[url("./assets/hero-bg.svg")] bg-cover bg-center px-12 sm:px-24'
     >
       <div className='flex w-full flex-col gap-6'>
-        <div className='flex w-full items-center justify-center pb-4 after:relative after:ml-4 after:block after:h-[1px] after:w-full after:bg-accent after:content-[""]'>
-          <h2 className='font-secondary min-w-36 text-lg font-bold sm:min-w-48 sm:text-2xl'>
-            <span className='text-accent'>03.</span> Contact
-          </h2>
-        </div>
+        <SectionTitle number='03.' title='Contact' />
         <p>Contact Me</p>
       </div>
-      <p className='font-secondary absolute bottom-10 text-xs text-secondary'>
+      <p className='absolute bottom-10 font-secondary text-xs text-secondary'>
         Designed & Built By Peter Do
       </p>
     </div>
