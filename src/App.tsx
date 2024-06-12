537965fec80db62e63d7422bfc45946d9e187563
Peter Do <peter.toan.do@gmail.com>
2024-06-12 11:47:20 -0400

    Adding background effect to body

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Contact from './components/Contact';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
 
+// bg-gradient-to-tl from-background via-foreground/5 to-background
+
 export default function App() {
   return (
-    <div className='bg-gradient-to-tl from-background via-foreground/5 to-background text-foreground'>
+    <div className='text-foreground'>
       <Hero />
       <About />
       <Projects />
